465ec69b2087bc72aeaceb327d68734298e11960
Megood1210 <xuanvinhle06@gmail.com>
2026-02-23 21:40:01 +0700

    Upload bài tập 1

---
+++ Bai1.java
@@ -0,0 +1,57 @@
+interface Shape {
+    double getArea();
+    double getPerimeter();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("=== HÌNH TRÒN ===");
+        System.out.println("Diện tích: " + circle.getArea());
+        System.out.println("Chu vi: " + circle.getPerimeter());
+
+        System.out.println("=== HÌNH CHỮ NHẬT ===");
+        System.out.println("Diện tích: " + rectangle.getArea());
+        System.out.println("Chu vi: " + rectangle.getPerimeter());
+    }
+}
